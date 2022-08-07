a1f3e1801775c78dd0f9ee13ed347e7b8d30aade
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-07 20:55:26 +0300

    Structuring Project

--- rtk-crud/src/App.tsx
+++ rtk-crud/src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import AddEdit from "./pages/AddEdit";
+import Home from "./pages/Home";
+import Info from "./pages/Info";
 
 function App() {
   return (
     <div className="App">
-      <h2>RTK Query</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/AddContact" element={<AddEdit />} />
+          <Route path="/editContact/:id" element={<AddEdit />} />
+          <Route path="/info/:id" element={<Info />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
