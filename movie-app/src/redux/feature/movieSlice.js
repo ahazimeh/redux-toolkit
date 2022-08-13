c706e24863b34f57debea5e313efcdc26af89e6b
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-13 08:44:13 +0300

    Configure Redux-Toolkit & Redux-Saga

---
+++ movie-app/src/redux/feature/movieSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const movieSlice = createSlice({
+  name: "movie",
+  initialState: {
+    moviesList: [],
+    movie: {},
+  },
+});
+
+export default movieSlice.reducer;
