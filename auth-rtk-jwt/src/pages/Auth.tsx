6ece6f6de79d32c0af0f01ae06e3498c71432546
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-15 21:26:51 +0300

    Login Form

--- auth-rtk-jwt/src/pages/Auth.tsx
+++ auth-rtk-jwt/src/pages/Auth.tsx
@@ -1,7 +1,109 @@
+import { MDBInput } from "mdb-react-ui-kit";
+import { useState } from "react";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const Auth = () => {
+  const [formValue, setFormValue] = useState(initialState);
+  const { firstName, lastName, email, password, confirmPassword } = formValue;
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleChange = () => {};
   return (
     <>
-      <div>Auth</div>
+      <section className="vh-100 gradient-custom">
+        <div className="container py-4 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+              <div
+                className="card bg-dark text-white"
+                style={{ borderRadius: "1rem" }}
+              >
+                <div className="card-body p-4 text-center">
+                  <div className="mb-md-5 mt-md-4 pb-5">
+                    <h2 className="fw-bold mb-2 text-uppercase">
+                      {!showRegister ? "Login" : "Register"}
+                    </h2>
+                    <p className="text-white-50 mb-4">
+                      {!showRegister
+                        ? "Please enter your Email & Password"
+                        : "Please enter User details"}
+                    </p>
+                    <div className="form-outline form-white mb-4">
+                      <MDBInput
+                        type="text"
+                        name="email"
+                        value={email}
+                        onChange={handleChange}
+                        label="Email"
+                        className="form-control form-control-lg"
+                      />
+                    </div>
+                    <div className="form-outline form-white mb-4">
+                      <MDBInput
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={handleChange}
+                        label="Password"
+                        className="form-control form-control-lg"
+                      />
+                    </div>
+                    {!showRegister ? (
+                      <button
+                        className="btn btn-outline-light btn-lg px-5"
+                        type="button"
+                      >
+                        Login
+                      </button>
+                    ) : (
+                      <button
+                        className="btn btn-outline-light btn-lg px-5"
+                        type="button"
+                      >
+                        Register
+                      </button>
+                    )}
+                  </div>
+                  <div>
+                    <h5 className="mb-0">
+                      {!showRegister ? (
+                        <>
+                          Don't have an account ?
+                          <p
+                            className="text-white-50 fw-bold"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => setShowRegister(true)}
+                          >
+                            Sign Up
+                          </p>
+                        </>
+                      ) : (
+                        <>
+                          Already have an account ?
+                          <p
+                            className="text-white-50 fw-bold"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => setShowRegister(false)}
+                          >
+                            Sign In
+                          </p>
+                        </>
+                      )}
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
