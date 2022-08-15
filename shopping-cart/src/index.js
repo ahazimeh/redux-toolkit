3f4a9b48bbddc2d413ccf72d64654e3f8c09106f
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-15 10:15:00 +0300

    Persist Cart value using Redux-Persist

--- shopping-cart/src/index.js
+++ shopping-cart/src/index.js
@@ -6,12 +6,18 @@ import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- shopping-cart/src/redux/store.js
+++ shopping-cart/src/redux/store.js
@@ -1,9 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./feature/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage: storage,
+};
+const reducer = combineReducers({
+  cart: cartSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 const store = configureStore({
-  reducer: {
-    cart: cartSlice,
-  },
+  //   reducer: {
+  //     cart: cartSlice,
+  //   },
+  reducer: persistedReducer,
 });
 export default store;
