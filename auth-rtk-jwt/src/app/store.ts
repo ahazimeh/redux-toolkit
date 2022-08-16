e7efbbd691599c520537c9dc72f1344aed3b4681
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-17 00:02:39 +0300

    Creating Slice in our App

--- auth-rtk-jwt/src/app/store.ts
+++ auth-rtk-jwt/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { authApi } from "../services/authApi";
+import authReducer from "../features/authSlice";
 import { setupListeners } from "@reduxjs/toolkit/query/react";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
