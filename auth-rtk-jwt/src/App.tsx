8475beac61fbbf10c61cd10c8e9fe2841c38df98
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-17 08:16:12 +0300

    Persist User

--- auth-rtk-jwt/src/App.tsx
+++ auth-rtk-jwt/src/App.tsx
@@ -4,8 +4,16 @@ import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useAppDispatch } from "./app/hooks";
+import { useEffect } from "react";
+import { setUser } from "./features/authSlice";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  useEffect(() => {
+    dispatch(setUser(user));
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
