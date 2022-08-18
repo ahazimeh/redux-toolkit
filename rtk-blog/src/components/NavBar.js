30ad506f50c38983ef982d5a6e003331c284f96b
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-18 22:21:44 +0300

    Navbar & Spinner Component

--- rtk-blog/src/components/NavBar.js
+++ rtk-blog/src/components/NavBar.js
@@ -1,8 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarItem,
+  MDBNavbarNav,
+} from "mdb-react-ui-kit";
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <div>NavBar</div>
-    </>
+    <MDBNavbar expand="lg" dark bgColor="dark">
+      <MDBContainer fluid>
+        <MDBNavbarBrand tag="span" className="mb-0 h1 fw-bold">
+          RTK-Query Firebase Blog App
+        </MDBNavbarBrand>
+        <div style={{ float: "right", marginRight: "50px" }}>
+          <MDBNavbarNav className="mb-2 mb-lg-0" fullWidth={false}>
+            <MDBNavbarItem>
+              <p className="header-text" onClick={() => navigate("/")}>
+                Home
+              </p>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <p className="header-text" onClick={() => navigate("/create")}>
+                Create
+              </p>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </div>
+      </MDBContainer>
+    </MDBNavbar>
   );
 };
 
--- rtk-blog/src/components/Spinner.js
+++ rtk-blog/src/components/Spinner.js
@@ -1,7 +1,15 @@
+import { MDBSpinner } from "mdb-react-ui-kit";
+
 const Spinner = () => {
   return (
     <>
-      <div>Spinner</div>
+      <MDBSpinner
+        grow
+        className="me-2 mt-5"
+        style={{ width: "3rem", height: "3rem" }}
+      >
+        <span className="visually-hidden">Loading...</span>
+      </MDBSpinner>
     </>
   );
 };
