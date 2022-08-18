4ffdb6b52bfbc89ec1a89ad4e6b024b46e28cc13
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-18 08:52:02 +0300

    Enable Routing in App

--- rtk-blog/src/App.js
+++ rtk-blog/src/App.js
@@ -1,12 +1,26 @@
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import AddEditBlog from "./pages/AddEditBlog";
+import Detail from "./pages/Detail";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <h2>RTK Blog</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <ToastContainer position="top-center" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<AddEditBlog />} />
+          <Route path="/update/:id" element={<AddEditBlog />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
