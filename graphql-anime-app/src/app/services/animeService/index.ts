ca2ea44a8a5b01b2f0cecdf944c2608ef4014a91
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-20 23:00:53 +0300

    Writing Anime Service

--- graphql-anime-app/src/app/services/animeService/index.ts
+++ graphql-anime-app/src/app/services/animeService/index.ts
@@ -1,3 +1,22 @@
-class AnimeService {}
+import { apolloClient } from "../../graphql";
+import { FETCH_ANIME_LIST } from "./queries";
+
+class AnimeService {
+  async fetchAnimeList(page: Number, perPage = 12): Promise<any> {
+    //Promise<FetchAnimeList["page"]>
+    try {
+      const response = await apolloClient.query({
+        query: FETCH_ANIME_LIST,
+        variables: { page, perPage },
+      });
+      if (!response || !response.data) {
+        throw new Error("Something went wrong");
+      }
+      return response.data.Page;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
 
 export default new AnimeService();
