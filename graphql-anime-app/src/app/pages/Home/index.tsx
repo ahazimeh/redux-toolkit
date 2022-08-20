4a03b7d6c47ef4d4cf6adf6a67981a5bf405d7b5
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-08-20 23:23:19 +0300

    Dispatch Redux Action

--- graphql-anime-app/src/app/pages/Home/index.tsx
+++ graphql-anime-app/src/app/pages/Home/index.tsx
@@ -1,15 +1,28 @@
 import { useEffect } from "react";
+import { Dispatch } from "redux";
+import { useAppDispatch } from "../../hooks";
 import animeService from "../../services/animeService";
+import { setAnimeList } from "./homeSlice";
+
+const actionDispatch = (dispatch: Dispatch) => ({
+  setAnimeList: (page: any /*FetchAnimeList["Page"]*/) =>
+    dispatch(setAnimeList(page)),
+});
 
 const Home = () => {
+  const { setAnimeList } = actionDispatch(useAppDispatch());
   useEffect(() => {
     fetchAnime();
   }, []);
+
   const fetchAnime = async () => {
     const animeList = await animeService.fetchAnimeList(0).catch((err) => {
       console.log(err);
     });
-    console.log("anime List", animeList);
+    // console.log("anime List", animeList);
+    if (animeList) {
+      setAnimeList(animeList);
+    }
   };
   return (
     <>
